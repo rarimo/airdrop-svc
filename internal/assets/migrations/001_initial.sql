84f7d7687f9ad23c6d170e96ba68cea91613d72f
Maksym Hrynenko <mg06062003@gmail.com>
2024-04-22 17:39:46 +0300

    fix: broken migrations in stage; add: tx info in participants table

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,15 +1,10 @@
 -- +migrate Up
-CREATE TYPE tx_status AS ENUM ('pending', 'completed');
-
 CREATE TABLE participants
 (
     nullifier  text PRIMARY KEY,
     address    text                        NOT NULL,
-    status tx_status NOT NULL,
-    created_at timestamp without time zone NOT NULL default NOW(),
-    updated_at timestamp without time zone NOT NULL default NOW()
+    created_at timestamp without time zone NOT NULL default NOW()
 );
 
 -- +migrate Down
 DROP TABLE participants;
-DROP TYPE tx_status;
