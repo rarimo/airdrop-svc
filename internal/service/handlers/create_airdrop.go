5ec1e146c13b511f93d4e387016b3b30c5c22fe4
Maksym Hrynenko <mg06062003@gmail.com>
2024-06-05 19:21:46 +0300

    update: zkverifier kit version, verify proof with decoded rarimo addr

--- internal/service/handlers/create_airdrop.go
+++ internal/service/handlers/create_airdrop.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/cosmos/cosmos-sdk/types"
 	"github.com/rarimo/airdrop-svc/internal/data"
 	"github.com/rarimo/airdrop-svc/internal/service/requests"
 	zk "github.com/rarimo/zkverifier-kit"
 	"github.com/rarimo/zkverifier-kit/identity"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
+	"gitlab.com/distributed_lab/logan/v3"
 )
 
 // Full list of the OpenSSL signature algorithms and hash-functions is provided here:
@@ -38,7 +40,16 @@ func CreateAirdrop(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = Verifier(r).VerifyProof(req.Data.Attributes.ZkProof, zk.WithRarimoAddress(req.Data.Attributes.Address))
+	addr, err := types.AccAddressFromBech32(req.Data.Attributes.Address)
+	if err != nil {
+		Log(r).WithError(err).WithFields(logan.F{
+			"address": req.Data.Attributes.Address,
+		}).Error("Failed to decode hex ethereum address")
+		ape.RenderErr(w, problems.InternalError())
+		return
+	}
+
+	err = Verifier(r).VerifyProof(req.Data.Attributes.ZkProof, zk.WithEventData(addr.Bytes()))
 	if err != nil {
 		if errors.Is(err, identity.ErrContractCall) {
 			Log(r).WithError(err).Error("Failed to verify proof")
